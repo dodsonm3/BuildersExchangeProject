dd7b36dc0a166984a44bfa1f6dd2e923329bff2a
Mathew Dodson (School) <55009295+dodsonm3@users.noreply.github.com>
2020-12-19 21:18:58 -0500

    Add project files.

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BuildersExchangeProject.Startup))]
+namespace BuildersExchangeProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
