e924901c4f5750ad353ba42d43c8933065f1c025
Mathew Dodson (School) <55009295+dodsonm3@users.noreply.github.com>
2020-12-22 00:32:41 -0500

    Made Controllers

---
+++ Controllers/CATEGORiesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using BuildersExchangeProject.Models;
+
+namespace BuildersExchangeProject.Controllers
+{
+    public class CATEGORiesController : Controller
+    {
+        private SANDEntities db = new SANDEntities();
+
+        // GET: CATEGORies
+        public ActionResult Index()
+        {
+            return View(db.CATEGORIES.ToList());
+        }
+
+        // GET: CATEGORies/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CATEGORY cATEGORY = db.CATEGORIES.Find(id);
+            if (cATEGORY == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cATEGORY);
+        }
+
+        // GET: CATEGORies/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: CATEGORies/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "CAT_ID,CAT_NAME")] CATEGORY cATEGORY)
+        {
+            if (ModelState.IsValid)
+            {
+                db.CATEGORIES.Add(cATEGORY);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(cATEGORY);
+        }
+
+        // GET: CATEGORies/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CATEGORY cATEGORY = db.CATEGORIES.Find(id);
+            if (cATEGORY == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cATEGORY);
+        }
+
+        // POST: CATEGORies/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "CAT_ID,CAT_NAME")] CATEGORY cATEGORY)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(cATEGORY).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(cATEGORY);
+        }
+
+        // GET: CATEGORies/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CATEGORY cATEGORY = db.CATEGORIES.Find(id);
+            if (cATEGORY == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cATEGORY);
+        }
+
+        // POST: CATEGORies/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            CATEGORY cATEGORY = db.CATEGORIES.Find(id);
+            db.CATEGORIES.Remove(cATEGORY);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
