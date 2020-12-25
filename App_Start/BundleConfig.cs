93b218e5078b675029d9172150d692480f8b788a
Mathew Dodson (School) <55009295+dodsonm3@users.noreply.github.com>
2020-12-25 18:36:46 -0500

    Add Css Update Links Tab Update Models

--- App_Start/BundleConfig.cs
+++ App_Start/BundleConfig.cs
@@ -24,7 +24,8 @@ namespace BuildersExchangeProject
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/Links.css"));
         }
     }
 }
